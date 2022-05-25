049ccfb1e95d6196bf4b3ca4e808f315fab03c02
shishir-b <99744836+shishir-b@users.noreply.github.com>
2022-05-25 22:22:48 +0530

    arrange

--- src/program.js
+++ src/program.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app=express()
 const port =process.env.PORT || 3000
 
-const basePath = path.join(__dirname,'../public')
+const basePath = path.join(__dirname,'./public')
 
 app.use(express.static(basePath))
 app.listen(port,()=>{
